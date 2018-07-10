fe809cfbb41672f4ad91179680f45f8b826882ff
Terje Dahl <terje@andante.no>
2018-07-10 22:55:01 +0200

    Final version.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 
-(defproject no.andante.george/george-application  "2018.6-SNAPSHOT"
+(defproject no.andante.george/george-application  "2018.6"
 
   :description "George - Application"
   :url "https://bitbucket.org/andante-george/george-application"
